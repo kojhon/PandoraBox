550a580eafa22b58000f3bf7fe484d995737beb4
Evgeniy Kovrizhnikov <opiym007@gmail.com>
2012-04-26 16:28:25 +0600

    added action listener

--- java/demo/src/JavaUI.java
+++ java/demo/src/JavaUI.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
 
 /**
  * Created by IntelliJ IDEA.
@@ -9,7 +12,9 @@ import java.awt.*;
  * Time: 15:36
  * To change this template use File | Settings | File Templates.
  */
-public class JavaUI extends JFrame{
+public class JavaUI extends JFrame implements ActionListener{
+    JTable t;
+
     public void initialize(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -17,17 +22,34 @@ public class JavaUI extends JFrame{
         this.setLayout(new BorderLayout());
         JPanel tablePanel = new JPanel();
         tablePanel.setLayout(new BorderLayout());
-        tablePanel.add(new JTable(new WeatherTableModel()),BorderLayout.CENTER);
+        t = new JTable(new WeatherTableModel());
+        tablePanel.add(t,BorderLayout.CENTER);
         this.add(tablePanel, BorderLayout.NORTH);
+
 
         JPanel buttonPanel = new JPanel();
         //buttonPanel.setSize(100,10);
         buttonPanel.setLayout(new GridLayout());
         //buttonPanel.setSize(400,10);
-        buttonPanel.add(new JButton("Удалить"));
-        buttonPanel.add(new JButton("Вставить"));
+        JButton deleteButton = new JButton("Удалить");
+        buttonPanel.add(deleteButton);
+        JButton insertButton = new JButton("Вставить");
+        insertButton.addActionListener(this);
+        deleteButton.addActionListener(this);
+        buttonPanel.add(insertButton);
         this.add(buttonPanel);
 
         this.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        if (e.getActionCommand() == "Вставить"){
+            int rowCount = CityList.getInstance().size();
+            CityList.add((String)t.getValueAt(rowCount,0),(String)t.getValueAt(rowCount,1));
+            t.setModel(new WeatherTableModel());
+            t.repaint();
+        }else{
+
+        }
     }
 }
